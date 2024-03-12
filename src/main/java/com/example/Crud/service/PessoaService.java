dcb51cccc9e154fafc21bf2db467bdc57c42596d
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-03-12 14:20:34 -0300

    Pessoa Service

--- src/main/java/com/example/Crud/service/PessoaService.java
+++ src/main/java/com/example/Crud/service/PessoaService.java
@@ -3,6 +3,7 @@ package com.example.Crud.service;
 import java.util.List;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.checkerframework.checker.units.qual.A;
 import org.hibernate.internal.util.collections.ConcurrentReferenceHashMap.Option;
@@ -17,6 +18,8 @@ import jakarta.transaction.Transactional;
 
 public class PessoaService {
 
+    private static final Logger logger = Logger.getLogger(PessoaService.class.getName());
+
     public static final String PESSOA_NAO_ENCONTRADA = "Pessoa nao encontrada";
 
     
@@ -24,13 +27,16 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     @Transactional
-    public Pessoa criaPessoa(Pessoa pessoa){
+    public Pessoa criaPessoa(Pessoa pessoa, List<Endereco> enderecos){
+        pessoa.setEnderecos(enderecos);
+        logger.info("Criando pessoa {}:" + pessoa);
         return pessoaRepository.save(pessoa);
     }
 
 
     @Transactional
     public Pessoa atualizarPessoa(Long pessoaId, Pessoa pessoa) {
+        logger.info("Atualizando pessoa {}:" + pessoaId + " com os dados: " + pessoa);
         Optional<Pessoa> pessoaOptional = pessoaRepository.findById(pessoaId);
         return pessoaOptional.map(p -> {
                 p.setNome(pessoa.getNome());
@@ -43,6 +49,7 @@ public class PessoaService {
 
     @Transactional
     public Pessoa deletarPessoa(Long pessoaId) {
+        logger.info("Deletando pessoa com id {}:" + pessoaId);
         Optional<Pessoa> pessoaOptional = pessoaRepository.findById(pessoaId);
         return pessoaOptional.map(p -> {
                 pessoaRepository.delete(p);
@@ -53,17 +60,20 @@ public class PessoaService {
 
     @Transactional
     public Pessoa buscarPessoa(Long pessoaId) {
+        logger.info("Buscando pessoa com id {}:" + pessoaId);
         Optional<Pessoa> pessoaOptional = pessoaRepository.findById(pessoaId);
         return pessoaOptional.orElseThrow(() -> new RuntimeException(PESSOA_NAO_ENCONTRADA));
     }
 
     @Transactional
     public List<Pessoa> buscarTodasPessoas() {
+        logger.info("Buscando todas as pessoas");
         return pessoaRepository.findAll();
     }
 
     @Transactional
     public Pessoa atualizarEnderecoPessoa(Long pessoaId, List<Endereco> novosEnderecos) {
+    logger.info("Atualizando enderecos da pessoa com id {}:" + pessoaId);
     Optional<Pessoa> pessoaOptional = pessoaRepository.findById(pessoaId);
     
     return pessoaOptional.map(p -> {
@@ -74,6 +84,10 @@ public class PessoaService {
         .orElseThrow(() -> new RuntimeException(PESSOA_NAO_ENCONTRADA));
     }
 
+ 
+
+
+  
 
   
     
--- src/main/java/com/example/Crud/service/PessoaServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.Crud.service;
-
-public class PessoaServiceImpl {
-    
-}
